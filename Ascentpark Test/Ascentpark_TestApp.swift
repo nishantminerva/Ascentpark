0aeed9f4fd2327524291733e16c6dc2114a511da
Nishant Minerva <nishantcafe@gmail.com>
2021-09-25 19:28:31 +0530

    Initial Commit

---
+++ Ascentpark Test/Ascentpark_TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  Ascentpark_TestApp.swift
+//  Ascentpark Test
+//
+//  Created by Satyam Kumar on 25/09/21.
+//
+
+import SwiftUI
+
+@main
+struct Ascentpark_TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
