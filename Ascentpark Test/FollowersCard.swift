2041b5e653a6b72bd7d32cdcf28423f4be0fc366
Nishant Minerva <nishantcafe@gmail.com>
2021-09-28 13:40:38 +0530

    Adjusting

--- Ascentpark Test/FollowersCard.swift
+++ Ascentpark Test/FollowersCard.swift
@@ -15,51 +15,59 @@ struct FollowersCard : View {
             VStack(alignment: .leading) {
                 HStack{
                     ZStack{
+                        //                        Profile picture in the FollowersCard
                         Circle()
                             .fill(Color.yellow)
                             .frame(width: 72, height: 72)
-                        CustomImageView(urlString: item.profile_picture) // This is where you extract urlString from Model ( transaction.imageUrl)
+                        //                        CustomImage from Url.
+                        CustomImageView(urlString: item.profile_picture) 
                             .frame(width: 69, height: 69, alignment: .center)
                     }
                     VStack{
                         HStack{
+                            //                             Name
                             Text(item.first_name + " " + item.last_name)
                                 .foregroundColor(.black)
                                 .font(.custom("Poppins-SemiBold", size: 16))
                             Spacer()
                         }
                         HStack{
+                            //                            UserName
                             Text("@" + item.user_name)
                                 .foregroundColor(Color(.secondaryLabel))
                                 .font(.custom("Poppins-Regular", size: 10))
                             HStack{
-                            Text("" + String(item.follow))
-                                .foregroundColor(Color(.secondaryLabel))
-                                .font(.custom("Poppins-Bold", size: 10))
-                            Text("followers")
-                                .foregroundColor(Color(.secondaryLabel))
-                                .font(.custom("Poppins-Regular", size: 10))
+                                //                                Followers Count
+                                Text("" + String(item.follow))
+                                    .foregroundColor(Color(.secondaryLabel))
+                                    .font(.custom("Poppins-Bold", size: 10))
+                                Text("followers")
+                                    .foregroundColor(Color(.secondaryLabel))
+                                    .font(.custom("Poppins-Regular", size: 10))
                             }
                             Spacer()
                         }
                         HStack{
                             ZStack{
+                                //                                Tags
                                 Capsule()
                                     .fill(Color(red: 242 / 255, green: 242 / 255, blue: 242 / 255))
-                                    .frame(width: 184, height: 26)
+                                    .frame(height: 26, alignment: .trailing)
                                 HStack{
-                                ForEach(0..<4) { num in
-                                    if num != 2 {
-                                        CustomImageView(urlString: item.followings.user_tags[num].tag.icon)
-                                            .frame(width: 14, height: 14, alignment: .trailing)
-                                        Text(item.followings.user_tags[num].tag.name)
-                                            .foregroundColor(Color(.gray))
-                                            .font(.custom("Raleway-Regular", size: 12))
-                                            .frame(height: 14)
-                                        Spacer()
+                                    ForEach(0..<4) { num in
+                                        if num != 2 {
+                                            HStack{
+                                            CustomImageView(urlString: item.followings.user_tags[num].tag.icon)
+                                                .frame(width: 14, height: 14, alignment: .trailing)
+                                            Text(item.followings.user_tags[num].tag.name)
+                                                .foregroundColor(Color(.gray))
+                                                .font(.custom("Raleway-Regular", size: 12))
+                                                .frame(height: 14, alignment: .trailing)
+                                            }
+                                            
+                                        }
+//                                        Spacer(minLength: 9)
                                     }
-                                    
-                                }
                                 }
                             }
                             
@@ -67,8 +75,8 @@ struct FollowersCard : View {
                     }
                     .padding(.bottom, 15)
                     HStack{
-                            Image("Plus")
-                                .frame(width: 35, height: 35)
+                        Image("Plus")
+                            .frame(width: 35, height: 35)
                     }
                     Spacer()
                 }
